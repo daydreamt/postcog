bff3bbf9d1b0d92e03bbe79d0a89f51c4dba8dd1
daydreamt <d@daydreamt.net>
2014-01-16 01:33:15 +0100

    Choose a license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject encog-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU LGPL"
+            :url "http://www.gnu.org/licenses/lgpl-3.0-standalone.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.mikera/core.matrix "0.19.0"]
                  [net.mikera/vectorz-clj "0.18.0"]
